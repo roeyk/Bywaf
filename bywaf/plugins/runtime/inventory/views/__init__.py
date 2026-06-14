05d990be55dd15e5ef1ae679b6f982d8fa258f01
Roey Katz <roey.katz@gmail.com>
2026-06-13 21:36:28 -0400

    Remove inventory facts compatibility shim

--- bywaf/plugins/runtime/inventory/views/__init__.py
+++ bywaf/plugins/runtime/inventory/views/__init__.py
@@ -6,22 +6,14 @@ Used by:
 - tests and future plugins that need stable runtime helper behavior.
 """
 
-from .facts import (
-    banner_event_keys,
-    cert_event_keys,
-    path_event_keys,
-    render_banners_inventory,
-    render_certs_inventory,
-    render_paths_inventory,
-    render_routes_inventory,
-    render_screenshots_inventory,
-    render_shares_inventory,
-    route_event_keys,
-    screenshot_event_keys,
-    share_event_keys,
-)
+from .banners import banner_event_keys, render_banners_inventory
+from .certs import cert_event_keys, render_certs_inventory
 from .hosts import host_event_keys, render_hosts_inventory
+from .paths import path_event_keys, render_paths_inventory
+from .routes import render_routes_inventory, route_event_keys
+from .screenshots import render_screenshots_inventory, screenshot_event_keys
 from .services import service_event_keys, render_services_inventory
+from .shares import render_shares_inventory, share_event_keys
 from .web import render_wafs_inventory, render_web_inventory, waf_event_keys, web_event_keys
 
 __all__ = [
